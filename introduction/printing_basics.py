4a522a113c8f9621569cc63a1bbfae7e09512ef6
Daniel Jeganathan <danieljeganathan@Apples-MacBook-Air.local>
2025-05-05 15:56:36 +0530

    multiline print

--- introduction/printing_basics.py
+++ introduction/printing_basics.py
@@ -1,3 +1,15 @@
 print("Hello World")
 print("Hello","World")
 print("Hi","How","Are","You")
+print('hello world')
+print(1*1)
+print("""multi line statement.going to third line """)
+print("multi line statement.going to third line")
+print('''multi line statement.going going to third line''')
+#PRINT("HELLO WORLD")
+#print "hello world!"
+print(100)
+print(1+5)
+print(1/1)
+
+
